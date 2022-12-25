ad2948b5e4233330b5e6181e54e0d607e67d6a02
ann <annmohammed96@gmail.com>
2022-12-25 11:06:53 +0200

    initializing

---
+++ Bokhary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bokhary
+//
+//  Created by Ann mohammed on 25/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
